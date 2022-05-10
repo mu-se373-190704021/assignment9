d564e323841eb63771fd8a5c2e88fce12a996a4e
ardadeniz <98977105+ardadenizkucukcoban@users.noreply.github.com>
2022-05-10 23:28:46 +0300

    commit1

--- app/src/main/java/com/ardadeniz/assignment9/MainActivity.kt
+++ app/src/main/java/com/ardadeniz/assignment9/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.ardadeniz.assignment9
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cameraPermission()
+    }
+    fun takeVideo(view: View){
+        startActivity(Intent(MediaStore.ACTION_VIDEO_CAPTURE))
+    }
+
+    fun takeImage(view: View){
+        startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+    }
+
+    private fun cameraPermission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),48)
+        }
     }
 }
